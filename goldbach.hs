943cec8951858975accee81c6ddd1e26d4cab2a0
Raul Lorca+Alvaro Garcia <lorca.raul+derecerca@gmail.com>
2015-08-17 20:05:49 +0200

    green

--- goldbach.hs
+++ goldbach.hs
@@ -4,9 +4,10 @@ goldbach :: Int -> Maybe [Int]
 goldbach n = 
 	if n - first_prime `elem` primes
 	  then Just [first_prime, n - first_prime]
-	  else Nothing
-	where first_prime = (head primes) 
-
-
+	  else Just [second_prime, n - second_prime]
+      		
+	where first_prime = (head primes)
+	      second_prime = head (tail primes)
+    
 test_7 = goldbach 7 == Just [2, 5]
 test_8 = goldbach 8 == Just [3, 5]
