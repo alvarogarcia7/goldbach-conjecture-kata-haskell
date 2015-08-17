be3fe03d0d5cf1014c24bf8a6fa6420591087b07
Raul Lorca+Alvaro Garcia <lorca.raul+derecerca@gmail.com>
2015-08-17 19:53:10 +0200

    green: should only return the solution when both are prime

--- goldbach.hs
+++ goldbach.hs
@@ -1,8 +1,11 @@
 primes = [2, 3, 5, 7, 11, 13, 17]
 
-goldbach :: Int -> [Int]
-goldbach n = [first_prime, n - first_prime] where
-	  first_prime = (head primes) 
+goldbach :: Int -> Maybe [Int]
+goldbach n = 
+	if n - first_prime `elem` primes
+	  then Just [first_prime, n - first_prime]
+	  else Nothing
+	where first_prime = (head primes) 
 
 
-test_7 = goldbach 7 == [2, 5]
+test_7 = goldbach 7 == Just [2, 5]
