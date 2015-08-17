3a6840dc89af3780a61cc2f76d7687053487120f
Raul Lorca+Alvaro Garcia <lorca.raul+derecerca@gmail.com>
2015-08-17 19:40:16 +0200

    refactor: remove duplication calculating the first_prime

--- goldbach.hs
+++ goldbach.hs
@@ -1,7 +1,8 @@
 primes = [2, 3, 5, 7, 11, 13, 17]
 
 goldbach :: Int -> [Int]
-goldbach n = [(head primes), n - (head primes)]
+goldbach n = [first_prime, n - first_prime] where
+	  first_prime = (head primes) 
 
 
 test_7 = goldbach 7 == [2, 5]
