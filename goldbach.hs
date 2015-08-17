b60c37269f3bfaef7d8dbd8a910dc9c538d59cfb
Raul Lorca+Alvaro Garcia <lorca.raul+derecerca@gmail.com>
2015-08-17 20:26:46 +0200

    green: compute all solutions with the given primes

--- goldbach.hs
+++ goldbach.hs
@@ -2,15 +2,17 @@ primes = [2, 3, 5, 7, 11, 13, 17]
 
 goldbach :: Int -> Maybe [Int]
 goldbach n = 
-	if n - first_prime `elem` primes
-	  then Just [first_prime, n - first_prime]
-	  else if n - second_prime `elem` primes
-	  	then Just [second_prime, n - second_prime]
-	  	else Just [third_prime, n - third_prime]
-      		
-	where first_prime = primes !! 0
-	      second_prime = primes !! 1
-	      third_prime = primes !! 2
+	case (is_goldbach n primes) of 
+		 (Just _) -> (is_goldbach n primes)
+		 Nothing -> Nothing
+
+
+is_goldbach :: Int -> [Int] -> Maybe [Int]
+is_goldbach n primes = 
+	if n - prime `elem` primes
+	  then Just [prime, n - prime]
+	  else (is_goldbach n (tail primes))
+	where prime = (head primes)
     
 test_7 = goldbach 7 == Just [2, 5]
 test_8 = goldbach 8 == Just [3, 5]
